a372be65ba7e6396acb16d5e176346daabf794c6
SgtHero <futuress@gmx.net>
2016-05-20 14:23:23 +0200

    Store und PhotoGrid finalized

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router'
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                Ich bin das PhotoGrid
+                {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
             </div>
         )
     }
-})
+});
 
 export default PhotoGrid;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,9 +1,18 @@
 // Ein Reducer empfängt 2 Sachen: Die Action (also die Info was passiert ist und eine Kopie des aktuellen states
 
 function posts(state = [], action) { // das state = [] ist ein ES6 default Parameter
-    console.log("This post will hook up")
-    console.log(state, action);
-    return state;
+    switch(action.type) {
+        case 'INCREMENT_LIKES' :
+            console.log("Like erhöht");
+            const i = action.index;
+            return [
+                ...state.slice(0,i),
+                {...state[i], likes: state[i].likes +1},
+                ...state.slice(i + 1),
+            ]
+        default:
+            return state;
+    }
 }
 
 export default posts;
