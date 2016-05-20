f5f740da45e0d496c5d443292662fb8abf4672f2
SgtHero <futuress@gmx.net>
2016-05-20 09:34:13 +0200

    created Components for photogrid and single also created a store

---
+++ client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Single = React.createClass({
+    render() {
+        return (
+            <div className="single-photo">
+                Ich bin das Single.js
+            </div>
+        )
+    }
+})
+
+export default Single;
